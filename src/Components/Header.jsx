1a167616d2b4aa39bde9fe8bc33e9ba65057c629
Dinesh10731j <dinesh1122@gmail.com>
2024-06-06 22:02:20 +0545

    done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,24 @@ import React from 'react'
 const Header = () => {
   return (
    <header>
-    <section className=''>
-        
+    <section className='flex flex-row justify-between '>
+    <section>
+        <h1 className='font-bold'>c<span className='text-2xl tracking-wide'>o</span>dynn</h1>
+    </section>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Compiler</li>
+            <li>Versus Battles</li>
+            <li>Questions</li>
+            <li>Challenges</li>
+            <li>About</li>
+        </ul>
+    </nav>
+
+    <section>
+        <button className='bg-[]'>LOGIN</button>
+    </section>
     </section>
    </header>
   )
