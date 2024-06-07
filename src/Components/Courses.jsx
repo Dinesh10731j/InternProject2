08a1bcd26c18834ba8ade0d036cbe590a53aec8a
Dinesh10731j <dinesh1122@gmail.com>
2024-06-07 12:22:04 +0545

    done

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -9,7 +9,9 @@ const Courses = ({ SearchInput }) => {
 
   return (
     <section className='flex flex-row gap-7 py-5 px-7 flex-wrap justify-center items-center mt-12'>
-      {filteredCourses.map((items, index) => (
+    {
+        filteredCourses.length>0?(
+      filteredCourses.map((items, index) => (
         <div key={index} className='h-[300px] w-[300px] bg-[#ecf0f1] px-7 py-4 overflow-hidden border border-black'>
           <h1 className='text-center font-bold text-3xl py-4'>{items.Title}</h1>
           <p>{items.Description}</p>
@@ -21,11 +23,13 @@ const Courses = ({ SearchInput }) => {
               <i className="fa-solid fa-arrow-right"></i>
             </section>
             <section>
-              <img src={Locker} alt='Locker' className='h-[100px] w-[100px]' />
+              <img src={items.Image} alt='Locker' className='h-[100px] w-[100px]' />
             </section>
           </section>
         </div>
-      ))}
+      ))):(<section>
+        <h1 className='text-center font-bold tracking-wide'>Courses Not found ...</h1>
+      </section>)}
     </section>
   );
 };
